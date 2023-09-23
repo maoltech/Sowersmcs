95ccd08b6059705d55c0c87975703077c3095ccd
maoltech <hardeyshinnah.mo@gmail.com>
2023-09-23 13:15:36 -0400

    transaction and dand models

--- dand/models.py
+++ dand/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 import uuid
 # Create your models here.
-class Wallet(models.Model):
+class dand(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     balance = models.DecimalField()
-    transactionCounts = models.IntegerField()
+    dandCounts = models.IntegerField()
+    description = models.CharField()
 
 
 def __str__(self):
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+import uuid
+from core.models import User
 # Create your models here.
+
+class transaction(models.Model):
+    transactionId = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    type = models.CharField()
+    transactor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='User')
+    
