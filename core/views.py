da489052106d8ae8eba57626df5896d4aaae370a
maoltech <hardeyshinnah.mo@gmail.com>
2023-09-24 05:02:28 -0400

    signup

--- core/views.py
+++ core/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib import messages
+from .models import User
+
 # Create your views here.
 
 def index(request):
@@ -8,4 +12,25 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        phone = request.POST['phone']
+        password1 = request.POST['password1']
+
+        if password != password1:
+            messages.info(request, 'Password Does Not match')
+            return redirect('signup')
+        else:
+            if User.objects.filter(phone=phone).exists():
+                messages.info(request, 'Phone number already exist')
+                return redirect('signup') 
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exist ')
+                return redirect('signup') 
+            else:
+                user = User.objects.create_user(username=username, phone=phone, password=password)
+                user.save()
+    else:
+        return render(request, 'signup.html')
